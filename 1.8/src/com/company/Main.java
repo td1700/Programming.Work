68c715a8d5d3c02d574dfa4384e27dd3c2bf5f46
Elise <td1700@students.inghamisd.org>
2019-01-24 10:26:46 -0500

    intObjects - started

---
+++ 1.8/src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //You will need to do this assignment in Java (some of these classes do not exist in C#)
+        // Everything in this project should bethe same syntax in C# and Java aside from the print statements.
+        // (Java uses System.out.println() instead of Console.WriteLine())*/
+
+
+        // create a Random object called myRandom
+        Random myRandom = new Random();
+        // create a Random object called anotherRandomGenerator
+        Random anotherRandomGenerater  = new Random();
+        // call the nextInt method on the myRandom object.
+        myRandom.nextInt();
+        // print the value that is returned by the method call/
+        System.out.println(myRandom.nextInt(());
+        // /call the nextDouble method on the anotherRandomGenerator object.
+       double var = anotherRandomGenerater.nextDouble():
+        // print the value that is returned by the method call
+        System.out.println(var);
+        // call the nextInt method on the myRandom object.
+        // print the value that is returned by the method call
+        System.out.println(myRandom.nextInt());
+        // call the nextDouble method on the anotherRandomGenerator object.
+        // print the value that is returned by the method call
+        System.out.println(anotherRandomGenerater.nextDouble(());
+        // create a new ArrayList object called myList that contains integers using the line below:
+        ArrayList<Integer> myList = new ArrayList<>();
+        // call the add method on myList passing in the number 12
+        // print the result of the value returned by calling the size method on myList
+        // call the add method on myList passing in the number 14
+        // print the result of the value returned by calling the size method on myList
+        // print the result of the value returned by calling the get method on myList passing in 0 as the input
+        // print the result of the value returned by calling the get method on myList passing in 1 as the input
+        // call the set method on myList passing in 0 and 5 as the two inputs
+        // print the result of the value returned by calling the get method on myList passing in 0 as the input
+        // print the result of the value returned by calling the get method on myList passing in 1 as the input/
+        // /call the clear method on myList
+        // print the result of the value returned by calling the size method on myList
+        // create a Date object called currentDate
+        // create a Date object called anotherDate
+        // call the toString method on currentDate and put it into a string variable called curDateString
+        // print curDateString
+        // call the setTime method for anotherDate passing in 123478 as its input.
+        // call the before method on currentDate passing in anotherDate
+        // if this returns true print "current date is before another date",
+        // else if it returns false print "current date is after another date"
+    }
+}
